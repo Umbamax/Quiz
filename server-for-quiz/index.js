9b7f6b97f6a11de11e670ef5c96e8942175b1fd9
Ilia Turovchik <umbomaxia@gmail.com>
2022-11-08 17:06:04 +0300

    feat: start work with mongodb

--- server-for-quiz/index.js
+++ server-for-quiz/index.js
@@ -1,14 +1,36 @@
-import express from "express";
-import fs from "fs";
-import path from "path";
-import cors from "cors";
+// import express from "express";
+// import fs from "fs";
+// import path from "path";
+// import cors from "cors";
 
-let __dirname = path.resolve();
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const cors = require('cors')
+
+const {MongoClient} = require('mongodb')
+const { resolve } = require('path')
+const { rejects } = require('assert')
+const client = new MongoClient('mongodb+srv://test:Zaq1xsw2345@cluster0.y412clp.mongodb.net/?retryWrites=true&w=majority')
+const start = async ()=>{
+  try{
+    await client.connect()
+    console.log('connection sucsessfull')
+    
+  }catch(e){
+    console.log('connection not sucsessfull')
+    console.log(e)
+  }
+}
+
+start()
+
+let dirname = path.resolve();
 
 const app = express();
 const jsonParser = express.json();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(dirname + "/public"));
 app.use(cors());
 app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ limit: "500mb" }));
@@ -23,36 +45,52 @@ app.get("/api/users", function (req, res) {
 
 // получение данных по логину
 
-app.get("/api/users/:login&:pass", function (req, res) {
-  const login = req.params.login; // получаем login
-  const password = req.params.pass; // получаем login
+app.get("/api/users/:login&:pass", async function (req, res) {
+  const inputLogin = req.params.login; // получаем login
+  const inputPassword = req.params.pass; // получаем login
 
-  const content = fs.readFileSync(usersJSONPath, "utf8");
-  const users = JSON.parse(content);
-  let user = null;
-  // находим в массиве пользователя по login
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].login === login) {
-      user = users[i];
-      if (user.password !== password) {
-        return res.status(401).send();
-      }
-      break;
-    }
+  const users = client.db().collection('users')
+
+  const currUser = await users.findOne({login:inputLogin})
+
+  if (!currUser){
+     res.status(404).send();
+}
+
+  if(currUser.password !== inputPassword){
+    return res.status(401).send();
+  }else{
+    res.send(currUser)
   }
-  // отправляем пользователя
-  if (user) {
-    res.send(user);
-  } else {
-    res.status(404).send();
-  }
+
+
+
+  // const allUsers = await users.find().toArray()
+     
+  // let user = null;
+  // // находим в массиве пользователя по login
+  // for (let i = 0; i < users.length; i++) {
+  //   if (users[i].login === login) {
+  //     user = users[i];
+  //     if (user.password !== password) {
+  //       return res.status(401).send();
+  //     }
+  //     break;
+  //   }
+  // }
+  // // отправляем пользователя
+  // if (user) {
+  //   res.send(user);
+  // } else {
+  //   res.status(404).send();
+  // }
 });
 
 // получение отправленных данных
 
-app.post("/api/users", jsonParser, function (req, res) {
+app.post("/api/users", jsonParser, async function (req, res) {
   if (!req.body) return res.sendStatus(400);
-  console.log(req.body);
+  
 
   const userName = req.body.name;
   const userLogin = req.body.login;
@@ -60,29 +98,31 @@ app.post("/api/users", jsonParser, function (req, res) {
   const userPassword = req.body.password;
 
   let user = { name: userName, login: userLogin, email: userEmail, password: userPassword, isAdmin: false };
-
-  let data = fs.readFileSync(usersJSONPath, "utf8");
-  let users = JSON.parse(data);
+  const users = client.db().collection('users')
+  await users.insertOne(user)
+  
+  // let data = fs.readFileSync(usersJSONPath, "utf8");
+  // let users = JSON.parse(data);
   //поверяем на дубль пользователя
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].login === userLogin || users[i].email === userEmail) {
-      return res.status(409).send();
-    }
-  }
+  // for (let i = 0; i < users.length; i++) {
+  //   if (users[i].login === userLogin || users[i].email === userEmail) {
+  //     return res.status(409).send();
+  //   }
+  // }
   // находим максимальный id
-  const id = Math.max.apply(
-    Math,
-    users.map((o) => {
-      return o.id;
-    })
-  );
+  // const id = Math.max.apply(
+  //   Math,
+  //   users.map((o) => {
+  //     return o.id;
+  //   })
+  // );
   // увеличиваем его на единицу
-  user.id = id + 1;
+  // user.id = id + 1;
   // добавляем пользователя в массив
-  users.push(user);
-  data = JSON.stringify(users);
+  // users.push(user);
+  // data = JSON.stringify(users);
   // перезаписываем файл с новыми данными
-  fs.writeFileSync("users.json", data);
+  // fs.writeFileSync("users.json", data);
   res.send(user);
 });
 // удаление пользователя по id
